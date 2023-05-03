2f2922ea4f2ecb6421830a5b91b9d3da267c1374
EcheCostabel <exequielcostabel@gmail.com>
2023-05-03 19:52:02 -0300

    login and logout function

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -12,10 +12,17 @@ const initialState: AuthState = {
     validando: true,
     token: null,
     userName: '',
-    nombre: ''
+    nombre: '',
 };
 
-type AuthAction = { type: 'logout'}
+type LoginPayload = {
+    username: string,
+    nombre: string
+}
+
+type AuthAction = 
+    | { type: 'logout'}
+    | { type: 'login', payload: LoginPayload }
 
 const authReducer = ( state:AuthState, action: AuthAction ):AuthState => {
 
@@ -24,8 +31,16 @@ const authReducer = ( state:AuthState, action: AuthAction ):AuthState => {
             return {
                 validando: false,
                 token:null,
+                nombre: '',
                 userName: '',
-                nombre: ''
+            }
+
+        case 'login':
+            return {
+                validando: false,
+                token: 'ABC123',
+                nombre: action.payload.nombre,
+                userName: action.payload.username
             }
         
     }
@@ -41,6 +56,19 @@ export const Login = () => {
         }, 1500)
     }, []);
 
+    const login = () => {
+        dispatch({type: 'login', 
+        payload: {
+            nombre: 'Exequiel',
+            username: 'exe123'
+        }})
+    }
+
+    const logout = () => {
+        dispatch({type: 'logout'})
+    }
+
+
     if( state.validando) {
         return (
         <div className='alert alert-info'>
@@ -54,24 +82,23 @@ export const Login = () => {
     <>
         <h3>Login</h3>
 
-
-        <div className='alert alert-danger'>
-            No autenticado
-        </div>
-
-        <div className='alert alert-success'>
-            Autenticado
-        </div>
+        {
+            (state.token) //si tengo el token es porque estoy autenticado
+                ? <div className='alert alert-success'>Autenticado como {state.nombre}</div>
+                :<div className='alert alert-danger'>No autenticado</div>
+        }
 
 
+        {
+            (state.token)
+                ? (   
+                <button className='btn btn-danger' onClick={logout}>Logout</button>
+                )
+                :  (
+                <button className='btn btn-primary' onClick={login}>Login</button>
+                )
+        }
 
-        <button className='btn btn-primary'>
-            Login
-        </button>
-
-        <button className='btn btn-danger'>
-            Logout
-        </button>
     </> 
   )
 }
